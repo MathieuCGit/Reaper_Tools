dcde332d041a34854af143ee60183d10a2e7ebea
Mathieu CONAN <83604945+MathieuCGit@users.noreply.github.com>
2023-01-30 23:47:57 +0100

    Create MC_Vertical-Zoom_Minimize-folder_Keep-Track-Height-Locked_no-item-and-curosr-pos.lua

---
+++ Arrange View/MC_Vertical-Zoom_Minimize-folder_Keep-Track-Height-Locked_no-item-and-curosr-pos.lua
@@ -0,0 +1,122 @@
+-- @description Vertical zoom minimize folder keep track lock height doesn't care about cursor position (means operate for all arranvge view lenght) and doesn't care about items on track or not.
+-- @version 0.1
+-- @author Mathieu CONAN
+-- @about Author URI: https://forum.cockos.com/member.php?u=153781
+-- @licence GPL v3
+ 
+ --
+--[[ FUNCTIONS ]]--
+--
+
+	--- get the minimum track height on a project. This size is theme related so it may change from on theme to another.
+	-- this is a workaround by CFillion : [https://forum.cockos.com/showpost.php?p=2283520&postcount=17](https://forum.cockos.com/showpost.php?p=2283520&postcount=17)
+	function minimumTrackHeight()
+		local track = reaper.GetTrack(nil, 0)
+		
+		lockState=reaper.GetMediaTrackInfo_Value(track, "B_HEIGHTLOCK") 
+		trackHeight=reaper.GetMediaTrackInfo_Value(track, "I_TCPH")
+		
+		reaper.SetMediaTrackInfo_Value(track, "I_HEIGHTOVERRIDE", 1)
+		reaper.TrackList_AdjustWindows(true)
+		minimumHeight = reaper.GetMediaTrackInfo_Value(track, "I_TCPH")
+		
+		reaper.SetMediaTrackInfo_Value(track,"B_HEIGHTLOCK",lockState)
+		reaper.SetMediaTrackInfo_Value(track,"I_TCPH",trackHeight)
+		reaper.TrackList_AdjustWindows(true)
+		
+		return minimumHeight
+	end
+
+	--- This function use Julian Sader method to get arrange view height and width
+	function sizeOfArrangeView()
+		local _, left, top, right, bottom = reaper.JS_Window_GetClientRect( reaper.JS_Window_FindChildByID( reaper.GetMainHwnd(), 1000) )
+		widthOfArrangeView = right - left
+		heightOfArrangeView = bottom - top
+		return heightOfArrangeView, widthOfArrangeView
+	end
+	
+--
+--[[ CORE ]]--
+--
+
+function Main()
+
+	local nbrOfTracks=reaper.CountTracks(0)
+	local trackInfoArray={}
+	local minimumTrackHeight=minimumTrackHeight()
+	local nbrOfTrackWithItems=0
+	local nbrOfTrackWithOutItems=0
+	
+	for i=0,nbrOfTracks-1 do
+	
+		track=reaper.GetTrack( 0, i)
+		curPos=reaper.GetCursorPositionEx(0) --current edit cursor position
+
+		--get current track infos
+		lockToggle=reaper.GetMediaTrackInfo_Value( track, "B_HEIGHTLOCK" )--current track lock state
+		trackHeight=reaper.GetMediaTrackInfo_Value( track, "I_TCPH" )--current track height
+		trackNum=reaper.GetMediaTrackInfo_Value( track, "IP_TRACKNUMBER" ) --current track number
+		folderDepth=reaper.GetMediaTrackInfo_Value( track, "I_FOLDERDEPTH" ) --current track folder depth
+		nbrOfItems= reaper.CountTrackMediaItems( track ) --is there item on track
+		
+		if nbrOfItems > 0 then 
+			isThereItem = true
+			nbrOfTrackWithItems=nbrOfTrackWithItems+1
+		else 
+			isThereItem = false
+			nbrOfTrackWithOutItems=nbrOfTrackWithOutItems+1
+		end
+		
+		trackInfoArray[#trackInfoArray+1]=
+		{
+			trackNum=i+1, 
+			trackHeight=trackHeight, 
+			lockState=lockToggle, 
+			folderDepth=folderDepth, 
+			isThereItem=isThereItem
+		}
+	end
+	
+		
+	heightToRemove=math.floor(minimumTrackHeight*nbrOfTrackWithOutItems)
+	height,width=sizeOfArrangeView()
+	newHeight=height-heightToRemove
+	sizeOfEachTrack=math.floor(newHeight/nbrOfTrackWithItems)
+	
+	for i=0,nbrOfTracks-1 do
+		track=reaper.GetTrack( 0, i)
+			
+		if trackInfoArray[i+1]["folderDepth"] ~= 1 and trackInfoArray[i+1]["lockState"] ~= 1 then
+			reaper.SetMediaTrackInfo_Value( track, "I_HEIGHTOVERRIDE", sizeOfEachTrack)
+		else
+			reaper.SetMediaTrackInfo_Value( track, "I_HEIGHTOVERRIDE", 1)
+
+		end
+	end
+
+	--We need to update tracklist view in addition to update arrange
+	--function argument "isMinor=false" updates both TCP and MCP. "isMinor=true" updates TCP only. 
+	reaper.TrackList_AdjustWindows(true)
+end 
+ --
+--[[ EXECUTION ]]--
+--
+
+-- clear console debug
+reaper.ShowConsoleMsg("")
+
+reaper.PreventUIRefresh(1)
+
+-- Begining of the undo block. Leave it at the top of your main function.
+reaper.Undo_BeginBlock() 
+
+-- execute script core
+Main()
+ 
+-- End of the undo block. Leave it at the bottom of your main function.
+reaper.Undo_EndBlock("Vertical zoom minimize folder keep track lock height", - 1) 
+  
+-- update arrange view UI
+reaper.UpdateArrange()
+
+reaper.PreventUIRefresh(-1)
