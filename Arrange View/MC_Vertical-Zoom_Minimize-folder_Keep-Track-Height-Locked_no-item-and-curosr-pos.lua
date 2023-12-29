e8910a85222a7e7fd3355d827dac70293bffbb76
Mathieu CONAN <83604945+MathieuCGit@users.noreply.github.com>
2023-12-29 19:55:26 +0100

    UPDATE : Total rewriting
    now takes care of collapsed tracks, spacer and takes, locked tracks

--- Arrange View/MC_Vertical-Zoom_Minimize-folder_Keep-Track-Height-Locked_no-item-and-curosr-pos.lua
+++ Arrange View/MC_Vertical-Zoom_Minimize-folder_Keep-Track-Height-Locked_no-item-and-curosr-pos.lua
@@ -1,15 +1,13 @@
--- @description Vertical zoom minimizes folders, keeps track lock height, doesn't take care of cursor position (means operate for all arranvge view lenght) AND doesn't take care of tracks without items.
--- @version 0.6
+-- @description Vertical zoom minimizes folders, keeps track lock height, doesn't take care of cursor position (means operate for all arrange view lenght) AND doesn't take care of tracks without items.
+-- @version 1.0
 -- @author Mathieu CONAN
 -- @about Author URI: https://forum.cockos.com/member.php?u=153781
--- @changelog fix minimum track height calculation issue
 -- @licence GPL v3
--- @changelog Now takes care of spacer in total arrange view height (Reaper 7 update)
+-- @changelog Total rewriting. Now works with spacer, folder collapsed ("small and hidden" preferences only), takes locked track.
  
 --
 --[[ FUNCTIONS ]]--
 --
-
 	--- get the minimum track height on a project. This size is theme related so it may change from on theme to another.
 	-- this is a workaround by CFillion : [https://forum.cockos.com/showpost.php?p=2283520&postcount=17](https://forum.cockos.com/showpost.php?p=2283520&postcount=17)
 	function minimumTrackHeight()
@@ -51,92 +49,175 @@
 		heightOfArrangeView = bottom - top
 		return heightOfArrangeView, widthOfArrangeView
 	end
+
+	---This function returns the number of spacer in the project and the size of a spacer
+	function nbr_spacer()
+		local nbrOfTracks=reaper.CountTracks(0)
+		local nbrOfSpacer=0
+		
+		--with Reaper 7, spacer appears and have to be considered
+		retval, buf = reaper.get_config_var_string('trackgapmax') -- get the default spacer height in the preference ini file.
+		if not retval then
+			--if no spacer or reaper version under 7.0
+			spacerHeight=0
+		else
+			--get spacer defaut size
+			spacerHeight= tonumber(buf)
+		end
+		
+		for i=0, nbrOfTracks-1 do
+			
+			track=reaper.GetTrack( 0, i)
+			-- if there is a spacer above the current track
+			if reaper.GetMediaTrackInfo_Value( track, "I_SPACER" ) == 1 then
+				--we increment the number of spacer
+				nbrOfSpacer=nbrOfSpacer+1
+			end
+		end
+		
+		return nbrOfSpacer,spacerHeight
+	end
+	
+	---This function get the top level track folder from a given track even if there are other folders in the between
+	function get_top_level_track(track)
+		while true do
+			local parent = reaper.GetParentTrack(track)
+			if parent then
+				track = parent
+			else
+				return track
+			end
+		end
+	end
 	
 --
 --[[ CORE ]]--
 --
-
 function Main()
 
+	local nbr_tr_proj=reaper.CountTracks(0)
+	local is_in_collapsed=0
+	local tr_infos_array={}
+	local minimum_tr_height=minimum_tr_height()
+	local spacer_in_collapsed=0
+	
+--------------------------------------------------------------------------	
+	--[[ 		GET TRACKS INFORMATIONS IN A TABLE	]]--
+--------------------------------------------------------------------------	
+	for i=0,nbr_tr_proj-1 do
+	
+		tr=reaper.GetTrack( 0, i)
+		
+		--get current track infos		
+		tr_lock_state=reaper.GetMediaTrackInfo_Value( tr, "B_HEIGHTLOCK" )--current track lock state
+		tr_visible_state=reaper.GetMediaTrackInfo_Value( tr, "B_SHOWINTCP") --is it shown in TCP !!WARNING : output value is 0.0 or 1.0 NOT true of false
+		tr_height=reaper.GetMediaTrackInfo_Value( tr, "I_TCPH" )--current track height
+		nbr_items=reaper.CountTrackMediaItems(tr) --number of items on the current track
 
-	local nbrOfTracks=reaper.CountTracks(0)
-	local nbrOfVisibleTracks=0
-	local trackInfoArray={}
-	local minimumTrackHeight=minimumTrackHeight()
-	local nbrOfFolder=0
-	local nbrOfSpacer=0
-	
-	--with Reaper 7, spacer appears and have to be considered
-	retval, buf = reaper.get_config_var_string('trackgapmax') -- get the default spacer height in the preference ini file.
-	if not retval then
-		--if no spacer or reaper version under 7.0
-		spacerHeight=0
-	else
-		--get spacer defaut size
-		spacerHeight= tonumber(buf)
-	end
-	
-	for i=0,nbrOfTracks-1 do
-	
-		track=reaper.GetTrack( 0, i)
-
-		--get current track infos
-		lockToggle=reaper.GetMediaTrackInfo_Value( track, "B_HEIGHTLOCK" )--current track lock state
-		trackHeight=reaper.GetMediaTrackInfo_Value( track, "I_TCPH" )--current track height
-		trackNum=reaper.GetMediaTrackInfo_Value( track, "IP_TRACKNUMBER" ) --current track number
-		folderDepth=reaper.GetMediaTrackInfo_Value( track, "I_FOLDERDEPTH" ) --current track folder depth
-		 --is it shown in TCP !!WARNING : output value is 0.0 or 1.0 NOT true of false
-		isVisibleTCP=reaper.GetMediaTrackInfo_Value( track, "B_SHOWINTCP")
-		
-	
-		trackInfoArray[#trackInfoArray+1]=
-		{
-			trackNum=i+1, 
-			trackHeight=trackHeight, 
-			lockState=lockToggle, 
-			folderDepth=folderDepth,
-			isVisibleTCP=isVisibleTCP
-		}
-		
-		if isVisibleTCP == 1.0 then
-			nbrOfVisibleTracks=nbrOfVisibleTracks+1
-		end
-		
-		if folderDepth == 1 and isVisibleTCP == 1.0 then
-			nbrOfFolder = nbrOfFolder+1
-		end
-		-- if there is a spacer above the current track
-		if reaper.GetMediaTrackInfo_Value( track, "I_SPACER" ) == 1 then
-			--we increment the number of spacer
-			nbrOfSpacer=nbrOfSpacer+1
-		end		
-	end
-	
-	totalFolderHeight=nbrOfFolder*minimumTrackHeight
-	totalSpacerHeight=spacerHeight*nbrOfSpacer
-	
-	heightToRemove=totalFolderHeight+totalSpacerHeight
-	
-	height,width=sizeOfArrangeView()
-	height=height-heightToRemove
-	--we get the size of each track
-	sizeOfEachTrack=math.floor(height/(nbrOfVisibleTracks-nbrOfFolder))
-	
-	for i=0,nbrOfTracks-1 do
-		track=reaper.GetTrack( 0, i)
-		
-		if trackInfoArray[i+1]["folderDepth"] ~= 1 and trackInfoArray[i+1]["lockState"] ~= 1 and trackInfoArray[i+1]["isVisibleTCP"] == 1.0 then
-			reaper.SetMediaTrackInfo_Value( track, "I_HEIGHTOVERRIDE", sizeOfEachTrack)
+		top_level_tr=get_top_level_track(tr) --find the top level folder if exists
+		parent= reaper.GetParentTrack(tr) --get the parent folder track
+		if parent then
+		--if parent folder track exists
+			if reaper.GetMediaTrackInfo_Value( top_level_tr, "I_FOLDERCOMPACT" ) > 0 or reaper.GetMediaTrackInfo_Value( parent, "I_FOLDERCOMPACT" ) > 0 then
+			-- if there is a parent track and this parent track is a collapsed folder or the top level track is a collapsed folder
+				is_in_collapsed=1
+				if reaper.GetMediaTrackInfo_Value( tr, "I_SPACER" ) == 1 then
+					spacer_in_collapsed=spacer_in_collapsed+1
+				end
+			end
 		else
-			reaper.SetMediaTrackInfo_Value( track, "I_HEIGHTOVERRIDE", 1)
+			is_in_collapsed=0
+		end
+		
+		tr_infos_array[#tr_infos_array+1]=
+		{	
+			tr_lock_state=tr_lock_state,
+			tr_visible_state=tr_visible_state,
+			tr_height=tr_height,
+			is_in_collapsed=is_in_collapsed,
+			nbr_items=nbr_items
+		}	
+	end
 
+--------------------------------------------------------------------------	
+	--[[ 		COUNT VISIBLE TRACKS	]]--
+--------------------------------------------------------------------------
+	local tr_count=0
+	local tr_vis_no_item=0
+	for i=1, #tr_infos_array do
+		if tr_infos_array[i]["tr_lock_state"] == 0.0 and
+			tr_infos_array[i]["tr_visible_state"] == 1.0 and
+			tr_infos_array[i]["is_in_collapsed"] == 0 and
+			tr_infos_array[i]["nbr_items"] > 0 then
+				
+				--nbr of track not locked, visible, not in a collapsed folder and with at least one item
+				tr_count=tr_count+1
+		elseif tr_infos_array[i]["tr_visible_state"] == 1.0 and 
+				tr_infos_array[i]["nbr_items"] == 0 and
+				tr_infos_array[i]["is_in_collapsed"] == 0 then
+				
+					--nbr of track visible but without items
+					tr_vis_no_item=tr_vis_no_item+1
 		end
 	end
 
+--------------------------------------------------------------------------	
+	--[[ 		GET SPACERS INFORMATIONS 	]]--
+--------------------------------------------------------------------------	
+
+	--get spacer numbers and default spacer height
+	total_spacers,spacer_height=nbr_spacer()
+	
+	--remove spacers that are inside collapsed folders from total spacer count
+	total_spacers=total_spacers-spacer_in_collapsed
+	
+--------------------------------------------------------------------------	
+	--[[ 		GET ARRANGE VIEW SIZE	]]--
+--------------------------------------------------------------------------
+	--we get the arrangeview size (height and width)
+	height,width=sizeOfArrangeView()
+	
+	--remove visible track with no items height
+	height=height-(tr_vis_no_item*minimum_tr_height)
+
+	--get total height lock for track height locked and remove it from height
+	local total_locked_height=0
+	for i=1,#tr_infos_array do
+		track=reaper.GetTrack( 0, i-1)	
+		if tr_infos_array[i]["tr_lock_state"] == 1.0 then
+			total_locked_height=total_locked_height+tr_infos_array[i]["tr_height"]
+		end
+	end
+	height=height-total_locked_height
+	
+	--we remove the spacers height from height
+	height=height-(total_spacers*spacer_height)
+	
+	--we divide the height of the arrange view by the track count
+	sizeOfEachTrack=math.floor(height/tr_count)
+
+--------------------------------------------------------------------------	
+	--[[ 		APPLY NEW TRACK HEIGHT	]]--
+--------------------------------------------------------------------------	
+	--resizing each track regarding state lock and other criteras
+	for i=1,#tr_infos_array do
+		track=reaper.GetTrack( 0, i-1)	
+		if tr_infos_array[i]["tr_lock_state"] == 0.0 and
+			tr_infos_array[i]["tr_visible_state"] == 1.0 and
+			tr_infos_array[i]["is_in_collapsed"] == 0 and
+			tr_infos_array[i]["nbr_items"] > 0 then
+			
+			--if track is not locked, is visible in TCP, is not in collapsed folder and has at least one item we apply the new track height
+			reaper.SetMediaTrackInfo_Value( track, "I_HEIGHTOVERRIDE", sizeOfEachTrack)
+		elseif tr_infos_array[i]["tr_lock_state"] == 0.0 then
+			reaper.SetMediaTrackInfo_Value( track, "I_HEIGHTOVERRIDE", 1)
+		end
+	end
 	--We need to update tracklist view in addition to update arrange
 	--function argument "isMinor=false" updates both TCP and MCP. "isMinor=true" updates TCP only. 
 	reaper.TrackList_AdjustWindows(true)
-end 
+end
+
  --
 --[[ EXECUTION ]]--
 --
