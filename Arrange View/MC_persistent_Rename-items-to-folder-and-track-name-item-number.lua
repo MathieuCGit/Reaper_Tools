7e4bf408fbed7f886f0a3ea9a684b1b09484a9e7
Mathieu CONAN <83604945+MathieuCGit@users.noreply.github.com>
2026-09-15 01:50:22 +0200

    UPDATE: Added real ON/OFF toggle support for Action List and toolbars

--- Arrange View/MC_persistent_Rename-items-to-folder-and-track-name-item-number.lua
+++ Arrange View/MC_persistent_Rename-items-to-folder-and-track-name-item-number.lua
@@ -1,126 +1,334 @@
 -- @description Rename items to folder and track name item position number in the track
--- @author Mathieu CONAN   
--- @version 0.3
--- @changelog:  Fix better rendering in renaming with underscore position.
--- @link Github repository https://github.com/MathieuCGit/MC_VariousScripts
--- @about This script aims to rename item with folder name (if track is in folder) - track name - item position in the track
---    # WARNING : it's a persistent script, continuously running in background.
+-- @author Mathieu CONAN
+-- @version 0.4
+-- @changelog:
+--   0.4 - Added real ON/OFF toggle support for Action List and toolbars.
+-- @link Github repository https://github.com/MathieuCGit/Reaper_Tools/tree/main
+-- @about
+--   This script renames items with:
+--      - folder name (if track is in a folder)
+--      - track name
+--      - item position number in the track
 --
---    ### USER OPTIONS
+--   The script runs continuously in the background.
 --
---    **``SHOW_PARENT_FOLDER``**
---    If this option is set to 1 folder name will appear in item name. 0 to not show the folder name
+--   It can be used as a toggle action in REAPER toolbars.
 --
---    **``SHOW_TRACK_NAME``**
---    If this option is set to 1 track name will appear in item name. 0 to not show the track name
+--   ### USER OPTIONS
 --
---    **``SHOW_ITEM_NBR``**
---    If this option is set to 1 item  number will appear in item name. 0 to not show the item number
+--   SHOW_PARENT_FOLDER
+--      1 = show parent folder name
+--      0 = don't show parent folder name
+--
+--   SHOW_TRACK_NAME
+--      1 = show track name
+--      0 = don't show track name
+--
+--   SHOW_ITEM_NBR
+--      1 = show item number
+--      0 = don't show item number
 --
 
 --
 --[[ USER OPTIONS ]]
 --
-SHOW_PARENT_FOLDER= 1
-SHOW_TRACK_NAME = 1
-SHOW_ITEM_NBR = 1
+
+SHOW_PARENT_FOLDER = 1
+SHOW_TRACK_NAME    = 1
+SHOW_ITEM_NBR      = 1
+
 
 --
---[[ FUNCTIONS ]]
+--[[ TOGGLE MANAGEMENT ]]
 --
-	-- Allow us to make the script toggled (on/off) in the action list. This way it can be use easier in toolbars
-	-- this function is a total and unshamed copy/paste from awesome Lokasenna - Track selection follows item selection
-	-- https://raw.githubusercontent.com/ReaTeam/ReaScripts/master/Items Properties/Lokasenna_Track selection follows item selection.lua
-	(function()
-		local _, _, sectionId, cmdId = reaper.get_action_context()
 
-		if sectionId ~= -1 then
-			--if script is running
-			reaper.SetToggleCommandState(sectionId, cmdId, 1)--set toggle state to On in action list
-			reaper.RefreshToolbar2(sectionId, cmdId) --set toggle State to On in toolbar
+-- Get the Action List context.
+--
+-- sectionId identifies the Action List section in which the script
+-- is running.
+--
+-- cmdId identifies this particular script/action.
+--
 
-				reaper.atexit(function()
-				--before script totaly stop
-				reaper.SetToggleCommandState(sectionId, cmdId, 0) --set toggle state to Off in action list
-				reaper.RefreshToolbar2(sectionId, cmdId)--set toggle State to Off in toolbar
-				end)
-		end
-	end)()
-	
+local _, _, sectionId, cmdId = reaper.get_action_context()
+
+
+-- This function changes the visual ON/OFF state of the action.
 --
---[[ CORE ]]--
+-- IMPORTANT:
+-- SetToggleCommandState() does NOT start or stop a script.
+-- It only tells REAPER how the action should be displayed.
 --
+-- The actual persistence of this script is handled by reaper.defer()
+-- below, as in the original script.
+
+local function SetToggleState(state)
+
+    if sectionId ~= -1 then
+
+        reaper.SetToggleCommandState(
+            sectionId,
+            cmdId,
+            state
+        )
+
+        reaper.RefreshToolbar2(
+            sectionId,
+            cmdId
+        )
+
+    end
+end
+
+
+-- When the script starts, show it as ON.
+
+SetToggleState(1)
+
+
+-- When the script is stopped by REAPER, make sure the action
+-- is displayed as OFF.
+
+reaper.atexit(function()
+
+    SetToggleState(0)
+
+end)
+
+
+--
+--[[ CORE ]]
+--
+
 function Main()
-	
-	for i=0,  reaper.CountTracks(0)-1 do --iterate throught project tracks
-	
-		track = reaper.GetTrack(0, i) --get current track info
-		nbrOfItemsOnTrack =  reaper.CountTrackMediaItems( track ) --get nbre of item for current track
 
-		if SHOW_PARENT_FOLDER == 1 then
-			-- we get the parent folder track name
-			parentTrack = reaper.GetParentTrack( track )
-			if parentTrack ~= nil then
-			--If track has a parent folder
-				_, parentFolderName = reaper.GetSetMediaTrackInfo_String(parentTrack, "P_NAME", "", false)-- get parent track name
-				parentFolderName = parentFolderName.."_"
-			else		
-				parentFolderName=""
-			end
-		else          
-			parentFolderName=""
-		end
-		
-		if SHOW_TRACK_NAME == 1 then
-			_, trackName = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false) --get current track name
-			-- add underscore before if we got FOLDER name
-			if SHOW_PARENT_FOLDER == 1 then  trackName = "_"..trackName end
-			if trackName == "" then 
-			--if track name is empty we use track number instead
-				trackName = reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER") --get current track number 
-				trackName = math.floor(trackName) -- change float value to integer
-			end
-		else
-			trackName=""
-		end
-				
-		finalName = parentFolderName..trackName
-		
-		
-		if nbrOfItemsOnTrack > 0 then
-			--rename each item only with track name
-			for j=0, nbrOfItemsOnTrack-1 do
-				item =  reaper.GetTrackMediaItem(track,j)--get each item on the track
-				take = reaper.GetActiveTake(item) --get the active take for eah item
-				if take ~= nil then     
-					if SHOW_ITEM_NBR == 1 then
-					_, stringNeedBig = reaper.GetSetMediaItemTakeInfo_String( take, "P_NAME", finalName.."_"..j+1, true )--we rename it only with trackName
-					else
-					_, stringNeedBig = reaper.GetSetMediaItemTakeInfo_String( take, "P_NAME", finalName, true )--we rename it only with trackName
-					end
-				end     
-			end          
-		end
-	end
+    ------------------------------------------------------------------
+    -- Iterate through all tracks in the project
+    ------------------------------------------------------------------
 
-	
-reaper.defer(Main)
+    for i = 0, reaper.CountTracks(0) - 1 do
+
+
+        ----------------------------------------------------------------
+        -- Get current track
+        ----------------------------------------------------------------
+
+        local track =
+            reaper.GetTrack(0, i)
+
+
+        ----------------------------------------------------------------
+        -- Number of items on this track
+        ----------------------------------------------------------------
+
+        local nbrOfItemsOnTrack =
+            reaper.CountTrackMediaItems(track)
+
+
+        ----------------------------------------------------------------
+        -- PARENT FOLDER NAME
+        ----------------------------------------------------------------
+
+        local parentFolderName = ""
+
+
+        if SHOW_PARENT_FOLDER == 1 then
+
+            -- Get parent folder track.
+
+            local parentTrack =
+                reaper.GetParentTrack(track)
+
+
+            if parentTrack ~= nil then
+
+                -- Get parent folder name.
+
+                _, parentFolderName =
+                    reaper.GetSetMediaTrackInfo_String(
+                        parentTrack,
+                        "P_NAME",
+                        "",
+                        false
+                    )
+
+
+                -- Add underscore after folder name.
+
+                parentFolderName =
+                    parentFolderName .. "_"
+
+            end
+        end
+
+
+        ----------------------------------------------------------------
+        -- TRACK NAME
+        ----------------------------------------------------------------
+
+        local trackName = ""
+
+
+        if SHOW_TRACK_NAME == 1 then
+
+            -- Get track name.
+
+            _, trackName =
+                reaper.GetSetMediaTrackInfo_String(
+                    track,
+                    "P_NAME",
+                    "",
+                    false
+                )
+
+
+            -- If folder names are enabled,
+            -- add underscore before track name.
+
+            if SHOW_PARENT_FOLDER == 1 then
+
+                trackName =
+                    "_" .. trackName
+
+            end
+
+
+            -- If the track has no name,
+            -- use its track number instead.
+
+            if trackName == "" then
+
+                trackName =
+                    reaper.GetMediaTrackInfo_Value(
+                        track,
+                        "IP_TRACKNUMBER"
+                    )
+
+
+                -- Convert floating-point track number
+                -- to integer.
+
+                trackName =
+                    math.floor(trackName)
+
+            end
+
+        end
+
+
+        ----------------------------------------------------------------
+        -- FINAL BASE NAME
+        ----------------------------------------------------------------
+
+        local finalName =
+            parentFolderName .. trackName
+
+
+        ----------------------------------------------------------------
+        -- RENAME ITEMS
+        ----------------------------------------------------------------
+
+        if nbrOfItemsOnTrack > 0 then
+
+            for j = 0, nbrOfItemsOnTrack - 1 do
+
+
+                --------------------------------------------------------
+                -- Get item
+                --------------------------------------------------------
+
+                local item =
+                    reaper.GetTrackMediaItem(
+                        track,
+                        j
+                    )
+
+
+                --------------------------------------------------------
+                -- Get active take
+                --------------------------------------------------------
+
+                local take =
+                    reaper.GetActiveTake(item)
+
+
+                if take ~= nil then
+
+
+                    ----------------------------------------------------
+                    -- Build final item name
+                    ----------------------------------------------------
+
+                    local itemName
+
+
+                    if SHOW_ITEM_NBR == 1 then
+
+                        itemName =
+                            finalName .. "_" .. (j + 1)
+
+                    else
+
+                        itemName =
+                            finalName
+
+                    end
+
+
+                    ----------------------------------------------------
+                    -- Rename active take
+                    ----------------------------------------------------
+
+                    reaper.GetSetMediaItemTakeInfo_String(
+                        take,
+                        "P_NAME",
+                        itemName,
+                        true
+                    )
+
+                end
+            end
+        end
+    end
+
+
+    ------------------------------------------------------------------
+    -- IMPORTANT:
+    --
+    -- This is what makes the script persistent.
+    --
+    -- REAPER calls Main() again after returning to its event loop.
+    ------------------------------------------------------------------
+
+    reaper.defer(Main)
+
 end
 
 
 --
---[[ EXECUTION ]]--
+--[[ EXECUTION ]]
 --
 
--- clear console debug
+-- Clear console debug output.
+
 reaper.ClearConsole()
+
+
+-- Prevent unnecessary UI refresh while processing.
 
 reaper.PreventUIRefresh(1)
 
--- execute script core
+
+-- Execute the main function.
+
 Main()
- 
--- update arrange view UI
+
+
+-- Update arrange view.
+
 reaper.UpdateArrange()
+
+
+-- Restore normal UI refresh.
 
 reaper.PreventUIRefresh(-1)
